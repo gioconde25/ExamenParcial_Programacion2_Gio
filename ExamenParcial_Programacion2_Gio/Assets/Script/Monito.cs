7304b574bf9f804c3d38e38a420fd7fa7d16c00c
weyon177 <weyon177@gmail.com>
2025-11-18 12:45:35 -0600

    0.1.3

--- ExamenParcial_Programacion2_Gio/Assets/Script/Monito.cs
+++ ExamenParcial_Programacion2_Gio/Assets/Script/Monito.cs
@@ -3,37 +3,67 @@ using UnityEngine;
 public class Monito : MonoBehaviour
 {
     [SerializeField]
-    private Transform groundCheck;
+    private Rigidbody rb;
 
     [SerializeField]
-    private float upForce;
+    private CapsuleCollider collider;
 
     [SerializeField]
-    private LayerMask ground;
+    private float jumpForce = 5f;
 
     [SerializeField]
-    private float radius;
+    private bool isGrounded = true;
 
-    private Rigidbody monitoRb;
+    [SerializeField]
+    private float normalHeight = 2f;
 
-    private void Start()
+    [SerializeField]
+    private float crouchHeight = 1f;
+
+    [SerializeField]
+    private float crouchSpeed = 10f;
+
+    private float targetHeight;
+
+    void Start()
     {
-        monitoRb = GetComponent<Rigidbody>();
+        if (!rb) rb = GetComponent<Rigidbody>();
+        if (!collider) collider = GetComponent<CapsuleCollider>();
+
+        targetHeight = normalHeight;
     }
 
-    private void Update()
+    void Update()
     {
-        bool isGrounded = Physics2D.OverlapCircle(groundCheck.position, radius, ground);
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            monitoRb.AddForce(Vector3.up * upForce);
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
         }
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            targetHeight = crouchHeight;
+        }
+        else
+        {
+            targetHeight = normalHeight;
+        }
+
+        float newHeight = Mathf.Lerp(collider.height, targetHeight, Time.deltaTime * crouchSpeed);
+
+        collider.height = newHeight;
+        collider.center = new Vector3(0, newHeight / 2f, 0);
+
+        transform.localScale = new Vector3(1, newHeight / normalHeight, 1);
     }
 
-    private void OnDrawGizmos()
+    private void OnCollisionEnter(Collision collision)
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(groundCheck.position, radius);
+        if (collision.contacts[0].normal.y > 0.5f)
+        {
+            isGrounded = true;
+        }
     }
 }
